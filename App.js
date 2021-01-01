9b485fee2804d3d6b7b34108d59bdbcf507ccfe6
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-01 20:40:28 +0100

    init all app screens

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import Home from './app/Screens/Home';
+import About from './app/Screens/About/About';
+import Home from './app/Screens/Home/Home';
+import Members from './app/Screens/Members/Members';
+import Notifications from './app/Screens/Notifications/Notifications';
+import Settings from './app/Screens/Settings/Settings';
 
 export default function App() {
   return (
     <View>
       <Text>Hello WORLD from , @ilies.space</Text>
       <Home />
+      <Members />
+      <Notifications />
+      <Settings />
+      <About />
     </View>
   );
 }
---
+++ app/Screens/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function About() {
+  return (
+    <View>
+      <Text>About SC</Text>
+    </View>
+  );
+}
