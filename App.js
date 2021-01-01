49ca85d407e718fbd9613fb7b6986e551867eadb
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-01 19:29:39 +0100

    init screens folder & Home screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Home from './app/Screens/Home';
 
 export default function App() {
   return (
     <View>
       <Text>Hello WORLD from , @ilies.space</Text>
+      <Home />
     </View>
   );
 }
