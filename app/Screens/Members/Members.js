ac17ce928779deb6e68c58417e9b388742939ad0
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-02 15:58:12 +0100

    memeber screen basic structure

--- app/Screens/Members/Members.js
+++ app/Screens/Members/Members.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
+import {memebersList} from '../../../temps/data';
 import Colors from '../../../theme/Colors';
 
 export default function Members() {
@@ -9,7 +11,77 @@ export default function Members() {
         backgroundColor: Colors.dark,
         flex: 1,
       }}>
-      <Text style={{color: Colors.light}}>Members Goes here</Text>
+      {/* Search Area  */}
+      <View style={{margin: '3%', flexDirection: 'row', alignItems: 'center'}}>
+        <TextInput
+          placeholder={'Search by name'}
+          style={{
+            borderWidth: 0.5,
+            borderColor: Colors.lightGrey,
+            color: Colors.light,
+            paddingHorizontal: 10,
+            borderRadius: 5,
+            flex: 4,
+          }}
+          placeholderTextColor={Colors.light}
+        />
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <TouchableOpacity>
+            <Text style={{color: Colors.main, paddingHorizontal: 10}}>ALL</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* Lst of memebers   */}
+      <View style={{marginVertical: 1, margin: '4%'}}>
+        {memebersList.map((item) => {
+          return (
+            <View
+              key={item.id}
+              style={{
+                backgroundColor: Colors.dark,
+                padding: 10,
+                paddingHorizontal: 20,
+                borderBottomWidth: 0.5,
+                borderBottomColor: Colors.grey,
+              }}>
+              <View
+                style={{
+                  marginVertical: 4,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <View style={{flex: 1}}>
+                  <Text
+                    style={{
+                      fontWeight: 'bold',
+                      fontSize: 16,
+                      color: Colors.light,
+                    }}>
+                    {item.name.length > 20
+                      ? item.name.substring(0, 20) + '...'
+                      : item.name}
+                  </Text>
+                  <Text style={{color: Colors.lightGrey, fontSize: 12}}>
+                    - {item.duration} days
+                  </Text>
+                </View>
+
+                <Image
+                  style={{
+                    width: 60,
+                    height: 60,
+                  }}
+                  source={{
+                    uri: item.img,
+                  }}
+                />
+              </View>
+            </View>
+          );
+        })}
+      </View>
     </View>
   );
 }
