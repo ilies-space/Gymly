a870746719522006e8e3f73a1127dbc156a8de38
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-02 21:42:23 +0100

    Addmember basic form

--- app/Screens/Members/addMember.js
+++ app/Screens/Members/addMember.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import Colors from '../../../theme/Colors';
+import {useNavigation} from '@react-navigation/native';
+import {goback} from '../../../theme/Icons';
 
 export default function addMember() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ADD member</Text>
+    <View
+      style={{
+        backgroundColor: Colors.dark,
+        flex: 1,
+      }}>
+      {/* Header */}
+      <View
+        style={{
+          flexDirection: 'row',
+          padding: '3%',
+          borderBottomWidth: 0.5,
+          borderColor: Colors.dark,
+          elevation: 3,
+          margin: '1%',
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          {goback}
+        </TouchableOpacity>
+        <Text
+          style={{
+            color: Colors.light,
+            fontWeight: 'bold',
+            flex: 1,
+            textAlign: 'center',
+            justifyContent: 'center',
+            marginRight: 25,
+            paddingTop: 1,
+          }}>
+          Add member
+        </Text>
+      </View>
+
+      <ScrollView>
+        <Text>ADD member</Text>
+      </ScrollView>
     </View>
   );
 }
