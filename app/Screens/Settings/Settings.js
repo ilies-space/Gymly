7e41b966713473d2cb65b396bbdc762307ddff6a
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-02 20:56:20 +0100

    Update Settings.js

--- app/Screens/Settings/Settings.js
+++ app/Screens/Settings/Settings.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import Colors from '../../../theme/Colors';
+import {useNavigation} from '@react-navigation/native';
+import {goback} from '../../../theme/Icons';
 
 export default function Settings() {
+  const navigation = useNavigation();
+
   return (
     <View
       style={{
         backgroundColor: Colors.dark,
         flex: 1,
+        // padding: '4%',
       }}>
-      <Text style={{color: Colors.light}}>Settings Goes here</Text>
+      {/* Header */}
+      <View
+        style={{
+          flexDirection: 'row',
+          padding: '3%',
+          borderBottomWidth: 0.5,
+          borderColor: Colors.dark,
+          elevation: 3,
+          margin: '1%',
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          {goback}
+        </TouchableOpacity>
+        <Text
+          style={{
+            color: Colors.light,
+            fontWeight: 'bold',
+            flex: 1,
+            textAlign: 'center',
+            justifyContent: 'center',
+            marginRight: 25,
+            paddingTop: 1,
+          }}>
+          Settings
+        </Text>
+      </View>
+
+      <ScrollView style={{paddingHorizontal: '4%'}}>
+        <View style={{paddingVertical: 20}} />
+
+        <Button title={'Chage gym name'} />
+        <View style={{paddingVertical: 20}} />
+        <Button title={'set a password'} />
+        <View style={{paddingVertical: 20}} />
+        <Button title={'extract databse exel'} />
+      </ScrollView>
     </View>
   );
 }
