d54b70cfbd131c44e2ef8c37bb36fbc619dca65a
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-13 15:44:14 +0100

    installing I18N for multilanguage support

--- android/app/src/main/java/com/gymly/MainApplication.java
+++ android/app/src/main/java/com/gymly/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
